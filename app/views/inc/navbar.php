55b37508842c936853339fcc5355101431da4e3b
Ed Link III <linkiii@gmail.com>
2020-04-13 10:59:47 -0400

    Add 'About' page

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,5 +1,5 @@
     <nav>
-      <a id="title" href="<?php echo URLROOT; ?>/posts/show"><?php echo SITENAME; ?></a>
+      <a id="title" href="<?php echo URLROOT; ?>/pages/about"><?php echo SITENAME; ?></a>
       <ul>
 <?php if(!isset($_SESSION['user_id'])): ?>
         <li><a href="<?php echo URLROOT; ?>/users/signup">Sign Up</a></li>
--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,3 +1,15 @@
 <?php require APPROOT . '/views/inc/header.php' ?>
 <h1><?php echo $data['title'] ?></h1>
+<p>This is a project I started to learn/relearn PHP/MySQL after only doing Front-end dev work for nearly 7 years.</p>
+<br/>
+<p>It initially started out as a sort of community bulliten board (hence corkboard), but has evolved into more of a Twitter clone.</p>
+<br />
+<hr />
+<h3>Credits</h3>
+<b>MVC Framework: </b> <a href="https://github.com/bradtraversy">TraversyMVC</a><br />
+<br />
+<b>SVG Icons: </b> <a href="https://www.flaticon.com">flaticon.com</a><i class="flaticon-new"></i><br />(<a href="http://creativecommons.org/licenses/by/3.0/">CC</a>: <a data-file="034-plus" href="https://www.flaticon.com/authors/pixel-perfect">Pixel perfect</a>, <a data-file="035-add-friend" href="http://www.freepik.com">Freepik</a>, <a data-file="009-profile" href="https://www.flaticon.com/authors/kiranshastry">Kiranshastry</a>, <a data-file="030-pen" href="https://www.flaticon.com/authors/icongeek26">Icongeek26</a>, <a data-file="032-floppy-disk" href="https://www.flaticon.com/authors/good-ware">Good Ware</a>, <a data-file="029-up-arrow" href="https://www.flaticon.com/authors/smashicons">Smashicons</a>, <a data-file="036-trash-can" href="https://www.flaticon.com/authors/those-icons">Those Icons</a>)<br />
+<br />
+<b>Base CSS: </b> <a href="https://andybrewer.github.io/mvp/">MVP CSS</a><br />
+<br />
 <?php require APPROOT . '/views/inc/footer.php' ?>
