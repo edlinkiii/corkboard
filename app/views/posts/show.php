acd1e24571f36290259627709b6a710ad7a6bc98
Ed Link III <linkiii@gmail.com>
2020-04-17 05:51:36 -0400

    Styling for Edit Post button

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -2,12 +2,14 @@
 <?php if(is_array($data['posts'])): ?>
   <?php foreach($data['posts'] as $post): ?>
   <article id="post_id-<?php echo $post->post_id; ?>">
+    <header>
   <?php if(isset($_SESSION['user_id']) && $post->user_id === $_SESSION['user_id']): ?>
-    <a class="edit-button" style="float: right;" href="<?php echo URLROOT; ?>/posts/edit/<?php echo $post->post_id; ?>"><b><i class="flaticon flaticon-pen"></i> Edit Post</b></a>
+      <a class="edit-button" style="float: right;" href="<?php echo URLROOT; ?>/posts/edit/<?php echo $post->post_id; ?>"><b><i class="flaticon flaticon-pen"></i><span> Edit Post</span></b></a>
   <?php endif; ?>
-    <img class="profile-pic-sm" src="<?php echo URLROOT; ?>/images/profile_pic/<?php echo $post->user_pic; ?>" />
-    <h3><a href="<?php echo URLROOT; ?>/users/profile/<?php echo $post->user_id; ?>"><?php echo $post->user_name; ?></a></h3>
-    <a class='show-post-link' href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->post_id; ?>"><?php echo date(DATETIME_FORMAT, strtotime($post->post_stamp)); ?></a>
+      <img class="profile-pic-sm" src="<?php echo URLROOT; ?>/images/profile_pic/<?php echo $post->user_pic; ?>" />
+      <h3><a href="<?php echo URLROOT; ?>/users/profile/<?php echo $post->user_id; ?>"><?php echo $post->user_name; ?></a></h3>
+      <a class='show-post-link' href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->post_id; ?>"><?php echo date(DATETIME_FORMAT, strtotime($post->post_stamp)); ?></a>
+    </header>
     <hr />
     <p><?php echo $post->post_body; ?></p>
     <hr />
