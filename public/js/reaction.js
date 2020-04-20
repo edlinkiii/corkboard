8450101c48366a2da2c045918ecf4db02ba31e9d
Ed Link III <linkiii@gmail.com>
2020-04-20 08:58:42 -0400

    Add check to verify user is logged in

--- public/js/reaction.js
+++ public/js/reaction.js
@@ -33,42 +33,53 @@ function clickReactionListener(e) {
   let container = e.target.parent();
   let postId = container.parent().parent().attr('id').substring(8);
 
-  if (container.hasClass(LIKE_COLOR_CLASS) || container.hasClass(DISLIKE_COLOR_CLASS)) {
-    if (container.hasClass(DISLIKE_COLOR_CLASS)) {
-      container
-        .find(ICON_TAG)
-        .removeClass(DISLIKE_ICON_CLASS)
-        .addClass(LIKE_ICON_CLASS);
+  ajax({
+    url: SESSION_CHECK_URL,
+    callback: (check) => {
+      if(check.user_id > 0) {
+        if (container.hasClass(LIKE_COLOR_CLASS) || container.hasClass(DISLIKE_COLOR_CLASS)) {
+          if (container.hasClass(DISLIKE_COLOR_CLASS)) {
+            container
+              .find(ICON_TAG)
+              .removeClass(DISLIKE_ICON_CLASS)
+              .addClass(LIKE_ICON_CLASS);
+          }
+      
+          container
+            .removeClass(LIKE_COLOR_CLASS)
+            .removeClass(DISLIKE_COLOR_CLASS)
+            .addClass(UNTOUCHED_COLOR_CLASS);
+      
+          setReaction(postId);
+        } else {
+          if ($q('article#post_id-' + postId + ' .reaction-panel')) {
+            // panel exists, close it
+            $q('article#post_id-' + postId + ' .reaction-panel').remove();
+          } else {
+            // create panel
+            let targetSelector = 'article#post_id-' + postId + ' .reaction-holder';
+            let reactionPanel = new Panel({
+              target: targetSelector,
+              attachToElement: targetSelector,
+              width: 62,
+              height: 38,
+              addClass: 'reaction-panel',
+            });
+            let reactionPanelEl = reactionPanel.instance;
+            $q(targetSelector).parentNode.appendChild(reactionPanelEl);
+            reactionPanelEl.html('<div class="reaction-panel-container"><span class='+LIKE_COLOR_CLASS+'><'+ICON_TAG+' class="'+ICON_CLASS+' '+LIKE_ICON_CLASS+'"></'+ICON_TAG+'></span>&nbsp;&nbsp;<span class='+DISLIKE_COLOR_CLASS+'><'+ICON_TAG+' class="'+ICON_CLASS+' '+DISLIKE_ICON_CLASS+'"></'+ICON_TAG+'></span></div>');
+            reactionPanel.show();
+            $q('article#post_id-' + postId + ' .reaction-panel '+ICON_TAG+'.'+LIKE_ICON_CLASS).addEventListener("click", likeListener, { once: true });
+            $q('article#post_id-' + postId + ' .reaction-panel '+ICON_TAG+'.'+DISLIKE_ICON_CLASS).addEventListener("click", dislikeListener, { once: true });
+          }
+        }
+      
+      }
+      else {
+        location.href = LOGIN_URL;
+      }
     }
-
-    container
-      .removeClass(LIKE_COLOR_CLASS)
-      .removeClass(DISLIKE_COLOR_CLASS)
-      .addClass(UNTOUCHED_COLOR_CLASS);
-
-    setReaction(postId);
-  } else {
-    if ($q('article#post_id-' + postId + ' .reaction-panel')) {
-      // panel exists, close it
-      $q('article#post_id-' + postId + ' .reaction-panel').remove();
-    } else {
-      // create panel
-      let targetSelector = 'article#post_id-' + postId + ' .reaction-holder';
-      let reactionPanel = new Panel({
-        target: targetSelector,
-        attachToElement: targetSelector,
-        width: 62,
-        height: 38,
-        addClass: 'reaction-panel',
-      });
-      let reactionPanelEl = reactionPanel.instance;
-      $q(targetSelector).parentNode.appendChild(reactionPanelEl);
-      reactionPanelEl.html('<div class="reaction-panel-container"><span class='+LIKE_COLOR_CLASS+'><'+ICON_TAG+' class="'+ICON_CLASS+' '+LIKE_ICON_CLASS+'"></'+ICON_TAG+'></span>&nbsp;&nbsp;<span class='+DISLIKE_COLOR_CLASS+'><'+ICON_TAG+' class="'+ICON_CLASS+' '+DISLIKE_ICON_CLASS+'"></'+ICON_TAG+'></span></div>');
-      reactionPanel.show();
-      $q('article#post_id-' + postId + ' .reaction-panel '+ICON_TAG+'.'+LIKE_ICON_CLASS).addEventListener("click", likeListener, { once: true });
-      $q('article#post_id-' + postId + ' .reaction-panel '+ICON_TAG+'.'+DISLIKE_ICON_CLASS).addEventListener("click", dislikeListener, { once: true });
-    }
-  }
+  });
 }
 
 $qa(".reaction-holder i").forEach((el) => {
