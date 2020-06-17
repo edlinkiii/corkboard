088e77d8b58e4c6a8d76f98c3399d1f5616880ce
Ed Link III <linkiii@gmail.com>
2020-06-17 09:49:49 -0400

    Change Search menu option to Tags

--- app/views/inc/sidebar.php
+++ app/views/inc/sidebar.php
@@ -1,6 +1,7 @@
       <ul id='sidebar'>
         <li><a id="all" href="<?php echo URLROOT; ?>/"><b><i class="fa fa-globe"></i><span class="sidebar-label"> All</span></b></a></li>
         <li><a id="notifications" href="<?php echo URLROOT; ?>/notifications"><b><i class="fa fa-bell"></i><span id="notification_alert"></span><span class="sidebar-label"> Notifications</span></b></a></li>
+        <li><a id="tags" href="<?php echo URLROOT; ?>/tags"><b><i class="fa fa-hashtag"></i><span class="sidebar-label"> Recent Tags</span></b></a></li>
         <li><a id="favorites" href="<?php echo URLROOT; ?>/favorites"><b><i class="fa fa-heart"></i><span class="sidebar-label"> Favorites</span></b></a></li>
         <li><a id="stalking" href="<?php echo URLROOT; ?>/posts/stalk"><b><i class="fa fa-binoculars"></i><span class="sidebar-label"> Stalking</span></b></a></li>
         <li><a id="my_profile" href="<?php echo URLROOT; ?>/users/profile"><b><i class="fa fa-user"></i><span class="sidebar-label"> My Profile</span></b></a></li>
